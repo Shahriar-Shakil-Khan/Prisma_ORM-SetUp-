8d0904bb3385574b7c431ef0b14d91fab987ffcd
Shahriar-Shakil-Khan <shahriarshakil28@gmail.com>
2026-05-25 22:26:04 +0600

    setUp

---
+++ crud.ts
@@ -0,0 +1,18 @@
+import { prisma } from "./lib/prisma";
+
+async function run(){
+    // const createUser = await prisma.user.create({
+
+    //     data: {
+    //     email: "elsttsa@prisma.io",
+    //     name: "Elstta Prisma",
+    // }, 
+
+    // })
+    // console.log(createUser)
+   
+
+ 
+}
+
+run()
